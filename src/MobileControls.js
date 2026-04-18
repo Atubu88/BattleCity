52d32ffa57e03d2ae8adccb0d662bfd509fdb7c6
Atubu88 <165842809+Atubu88@users.noreply.github.com>
2026-04-18 21:48:15 +0200

    Fix mobile controls fallback events

--- src/MobileControls.js
+++ src/MobileControls.js
@@ -1,6 +1,7 @@
 function MobileControls(keyboard) {
   this._keyboard = keyboard;
   this._activeKeys = {};
+  this._supportsPointer = !!window.PointerEvent;
   this._setup();
 }
 
@@ -29,20 +30,23 @@ MobileControls.prototype._bindHoldControl = function (elementId, key) {
   var self = this;
   var pointerId = null;
   var element = $('#' + elementId);
+  if (!element.length) {
+    return;
+  }
 
-  function onDown(event) {
+  function onDown(event, id) {
     if (pointerId !== null) {
       return;
     }
-    pointerId = event.pointerId;
+    pointerId = id;
     self._activeKeys[elementId] = key;
     element.addClass('active');
     self._keyboard.pressKey(key);
     event.preventDefault();
   }
 
-  function onUp(event) {
-    if (pointerId !== null && event.pointerId !== pointerId) {
+  function onUp(event, id) {
+    if (pointerId !== null && id !== null && id !== pointerId) {
       return;
     }
     pointerId = null;
@@ -52,25 +56,59 @@ MobileControls.prototype._bindHoldControl = function (elementId, key) {
     event.preventDefault();
   }
 
-  element.on('pointerdown', onDown);
-  element.on('pointerup pointercancel pointerleave', onUp);
+  if (this._supportsPointer) {
+    element.on('pointerdown', function (event) {
+      onDown(event, event.pointerId);
+    });
+    element.on('pointerup pointercancel pointerleave', function (event) {
+      onUp(event, event.pointerId);
+    });
+    return;
+  }
+
+  element.on('touchstart', function (event) {
+    var touch = event.originalEvent.changedTouches[0];
+    onDown(event, touch.identifier);
+  });
+  element.on('touchend touchcancel', function (event) {
+    var touch = event.originalEvent.changedTouches[0];
+    onUp(event, touch ? touch.identifier : null);
+  });
+  element.on('mousedown', function (event) {
+    onDown(event, 'mouse');
+  });
+  element.on('mouseup mouseleave', function (event) {
+    onUp(event, 'mouse');
+  });
 };
 
 MobileControls.prototype._bindTapControl = function (elementId, key) {
   var self = this;
   var element = $('#' + elementId);
+  if (!element.length) {
+    return;
+  }
 
-  element.on('pointerdown', function (event) {
+  function press(event) {
     element.addClass('active');
     self._keyboard.pressKey(key);
     event.preventDefault();
-  });
+  }
 
-  element.on('pointerup pointercancel pointerleave', function (event) {
+  function release(event) {
     element.removeClass('active');
     self._keyboard.releaseKey(key);
     event.preventDefault();
-  });
+  }
+
+  if (this._supportsPointer) {
+    element.on('pointerdown', press);
+    element.on('pointerup pointercancel pointerleave', release);
+    return;
+  }
+
+  element.on('touchstart mousedown', press);
+  element.on('touchend touchcancel mouseup mouseleave', release);
 };
 
 MobileControls.prototype._releaseAll = function () {
