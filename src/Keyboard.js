ae8ab53d400d2b86e65492cf305c8b4b57bb50da
Atubu88 <165842809+Atubu88@users.noreply.github.com>
2026-04-18 19:42:44 +0200

    Add responsive mobile touch controls for gameplay

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -22,19 +22,35 @@ Keyboard.Event.KEY_RELEASED = 'Keyboard.Event.KEY_RELEASED';
 Keyboard.prototype._listen = function () {
   var self = this;
   $(document).keydown(function (event) {
-    if (!self._keys[event.which]) {
-      self._keys[event.which] = true;
-      self._events.push({name: Keyboard.Event.KEY_PRESSED, key: event.which});
-    }
+    self.pressKey(event.which);
     event.preventDefault();
   });
   $(document).keyup(function (event) {
-    if (self._keys[event.which]) {
-      self._keys[event.which] = false;
-      self._events.push({name: Keyboard.Event.KEY_RELEASED, key: event.which});
-    }
+    self.releaseKey(event.which);
     event.preventDefault();
   });
+};
+
+Keyboard.prototype.pressKey = function (key) {
+  if (!this._keys[key]) {
+    this._keys[key] = true;
+    this._events.push({name: Keyboard.Event.KEY_PRESSED, key: key});
+  }
+};
+
+Keyboard.prototype.releaseKey = function (key) {
+  if (this._keys[key]) {
+    this._keys[key] = false;
+    this._events.push({name: Keyboard.Event.KEY_RELEASED, key: key});
+  }
+};
+
+Keyboard.prototype.releaseAll = function () {
+  for (var key in this._keys) {
+    if (this._keys.hasOwnProperty(key) && this._keys[key]) {
+      this.releaseKey(Number(key));
+    }
+  }
 };
 
 Keyboard.prototype.fireEvents = function () {
