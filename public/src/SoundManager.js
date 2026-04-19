65c596d2bec7a4dd6149368dd179086d32b80ae0
Atubu88 <165842809+Atubu88@users.noreply.github.com>
2026-04-19 18:11:03 +0200

    Make fallback audio creation lazy in SoundManager preload loop

--- public/src/SoundManager.js
+++ public/src/SoundManager.js
@@ -1,51 +1,117 @@
-var SoundManager = (function() {
-  var DEFAULT_SOUND_POOL_SIZE = 2;
-  var SOUND_POOL_SIZE_BY_KEY = {
-    bullet_shot: 6,
-  };
+var SoundManager = (function () {
   var sounds = {
-    stage_start: null,
-    game_over: null,
-    bullet_shot: null,
-    bullet_hit_1: null,
-    bullet_hit_2: null,
-    explosion_1: null,
-    explosion_2: null,
-    pause: null,
-    powerup_appear: null,
-    powerup_pick: null,
-    statistics_1: null,
+    stage_start: true,
+    game_over: true,
+    bullet_shot: true,
+    bullet_hit_1: true,
+    bullet_hit_2: true,
+    explosion_1: true,
+    explosion_2: true,
+    pause: true,
+    powerup_appear: true,
+    powerup_pick: true,
+    statistics_1: true,
   };
-  var soundPools = {};
-  
-  for (var i in sounds) {
-    var snd = new Audio("sound/" + i + ".ogg");
-    snd.preload = "auto";
-    sounds[i] = snd;
+  var AudioContextClass = window.AudioContext || window.webkitAudioContext;
+  var hasWebAudio = !!AudioContextClass;
+  var context = hasWebAudio ? new AudioContextClass() : null;
+  var buffers = {};
+  var fallbackSounds = {};
+  var loadingPromises = {};
 
-    soundPools[i] = [];
-    var poolSize = SOUND_POOL_SIZE_BY_KEY[i] || DEFAULT_SOUND_POOL_SIZE;
-    for (var j = 0; j < poolSize; j++) {
-      var pooledSound = new Audio("sound/" + i + ".ogg");
-      pooledSound.preload = "auto";
-      soundPools[i].push(pooledSound);
+  function createFallbackSound(key) {
+    if (!fallbackSounds[key]) {
+      var audio = new Audio("sound/" + key + ".ogg");
+      audio.preload = "auto";
+      fallbackSounds[key] = audio;
+    }
+    return fallbackSounds[key];
+  }
+
+  function fallbackPlay(key) {
+    var sound = createFallbackSound(key);
+    sound.currentTime = 0;
+    var playResult = sound.play();
+    if (playResult && playResult.catch) {
+      playResult.catch(function () {});
     }
   }
-  
-  return {
-    play: function (sound) {
-      var pool = soundPools[sound];
-      var playableSound = sounds[sound];
 
-      for (var i = 0; i < pool.length; i++) {
-        if (pool[i].paused || pool[i].ended) {
-          playableSound = pool[i];
-          break;
+  function decodeWithContext(arrayBuffer) {
+    return new Promise(function (resolve, reject) {
+      context.decodeAudioData(arrayBuffer, resolve, reject);
+    });
+  }
+
+  function loadBuffer(key) {
+    if (!hasWebAudio) {
+      return Promise.resolve(null);
+    }
+    if (buffers[key]) {
+      return Promise.resolve(buffers[key]);
+    }
+    if (loadingPromises[key]) {
+      return loadingPromises[key];
+    }
+    loadingPromises[key] = fetch("sound/" + key + ".ogg")
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error("Failed to load sound: " + key);
         }
+        return response.arrayBuffer();
+      })
+      .then(decodeWithContext)
+      .then(function (buffer) {
+        buffers[key] = buffer;
+        delete loadingPromises[key];
+        return buffer;
+      })
+      .catch(function () {
+        delete loadingPromises[key];
+        return null;
+      });
+    return loadingPromises[key];
+  }
+
+  function resumeContextIfNeeded() {
+    if (!hasWebAudio || !context || context.state !== "suspended") {
+      return Promise.resolve();
+    }
+    return context.resume().catch(function () {});
+  }
+
+  function playWithWebAudio(key, buffer) {
+    if (!buffer) {
+      fallbackPlay(key);
+      return;
+    }
+    var source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start(0);
+  }
+
+  for (var sound in sounds) {
+    if (sounds.hasOwnProperty(sound)) {
+      loadBuffer(sound);
+    }
+  }
+
+  return {
+    play: function (key) {
+      if (!sounds[key]) {
+        return;
+      }
+      if (!hasWebAudio) {
+        fallbackPlay(key);
+        return;
       }
 
-      playableSound.currentTime = 0;
-      playableSound.play();
+      resumeContextIfNeeded().then(function () {
+        loadBuffer(key).then(function (buffer) {
+          playWithWebAudio(key, buffer);
+        });
+      });
     },
   };
 })();
